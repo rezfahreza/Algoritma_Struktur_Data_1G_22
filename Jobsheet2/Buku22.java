6bb91b1d78dd51a4de4230c4cc263ef6d9ca2867
rezfahreza <143343397+rezfahreza@users.noreply.github.com>
2024-02-20 10:44:37 +0700

    P2_Latihan Praktikum_22

--- Jobsheet2/Buku22.java
+++ Jobsheet2/Buku22.java
@@ -1,28 +1,6 @@
 public class Buku22 {
     String judul, pengarang;
-    int halaman, stok, harga;
-
-    void tampilInformasi() {
-        System.out.println("Judul: " + judul);
-        System.out.println("Pengarang: " + pengarang);
-        System.out.println("Jumlah halaman: " + halaman);
-        System.out.println("Sisa stok: " + stok);
-        System.out.println("Harga: Rp" + harga);
-    }
-
-    void terjual(int jml) {
-        if (stok > 0) {
-            stok -= jml;
-        }
-    }
-
-    void restock(int jml) {
-        stok += jml;
-    }
-
-    void gantiHarga(int hrg) {
-        harga = hrg;
-    }
+    int halaman, stok, harga, terjual;
 
     public Buku22(){
         
@@ -34,5 +12,55 @@ public class Buku22 {
         halaman = hal;
         this.stok = stok;
         harga = har;
+    }
+
+    void tampilInformasi() {
+        System.out.println("Judul: " + judul);
+        System.out.println("Pengarang: " + pengarang);
+        System.out.println("Jumlah halaman: " + halaman);
+        System.out.println("Sisa stok: " + stok);
+        System.out.println("Harga: Rp" + harga);
+        System.out.println("Harga Buku: "+hitungHargaTotal());
+        System.out.println("Diskon: "+hitungDiskon());
+        System.out.println("Total: "+hitungHargaBayar());
+    }
+
+    void terjual(int jml) {
+        if (stok > 0) {
+            stok -= jml;
+            terjual=jml;
+        }
+    }
+
+    void restock(int jml) {
+        stok += jml;
+    }
+
+    void gantiHarga(int hrg) {
+        harga = hrg;
+    }
+
+    int hitungHargaTotal(){
+        int harTot = terjual*harga;
+        return harTot;
+    }
+
+    double hitungDiskon(){
+        if (hitungHargaTotal() > 150000) {
+            double diskon = hitungHargaTotal()*0.12;
+            return diskon;
+        } else if ((hitungHargaTotal() > 75000)&&(hitungHargaTotal() <= 150000)) {
+            double diskon = hitungHargaTotal()*0.05;
+            return diskon;
+        } else {
+            double diskon = hitungHargaTotal() * 0;
+            return diskon;
+        }
+    }
+
+    double hitungHargaBayar(){
+        double hargaBayar = hitungHargaTotal() - hitungDiskon();
+        int hargaInt = (int)hargaBayar;
+        return hargaInt;
     }
 }
