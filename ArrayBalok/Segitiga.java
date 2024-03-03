1fba00d371d6140ea49583384f8a5b106a6bb1ee
rezfahreza <143343397+rezfahreza@users.noreply.github.com>
2024-03-04 00:13:18 +0700

    P3_Percobaan3_Pertanyaan2_22

---
+++ ArrayBalok/Segitiga.java
@@ -0,0 +1,13 @@
+package ArrayBalok;
+
+public class Segitiga {
+    public int alas;
+    public int tinggi;
+
+    public Segitiga(int a, int t){
+        alas=a;
+        tinggi=t;
+    }
+
+    
+}
