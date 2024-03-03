cc020e3db6334e1ff950abdba67ad6999ac0c873
rezfahreza <143343397+rezfahreza@users.noreply.github.com>
2024-03-04 00:14:42 +0700

    P3_Percobaan3_Pertanyaan4_22

---
+++ ArrayBalok/Segitigamain.java
@@ -0,0 +1,13 @@
+package ArrayBalok;
+
+public class Segitigamain {
+    public static void main(String[] args) {
+        Segitiga[] sgArray = new Segitiga[4];
+        sgArray[0] = new Segitiga(10, 4);
+        sgArray[1] = new Segitiga(20, 10);
+        sgArray[2] = new Segitiga(15, 6);
+        sgArray[3] = new Segitiga(25, 10);
+
+        
+    }
+}
