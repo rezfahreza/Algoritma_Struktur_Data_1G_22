007ec21c2e222059be0d3dbb981cb107656ec03c
rezfahreza <143343397+rezfahreza@users.noreply.github.com>
2024-03-03 23:32:06 +0700

    P3_Percobaan2_Pertanyaan4_22

--- Jobsheet3/ArrayObject22.java
+++ Jobsheet3/ArrayObject22.java
@@ -2,10 +2,11 @@ package Jobsheet3;
 import java.util.Scanner;
 public class ArrayObject22 {
     public static void main(String[] args) {
-        persegipanjang[] ppArray = new persegipanjang[3];
         Scanner sc22 = new Scanner(System.in);
+        int n = sc22.nextInt();
+        persegipanjang[] ppArray = new persegipanjang[n];
         
-        for (int i = 0; i < 3; i++) {
+        for (int i = 0; i < n; i++) {
             ppArray[i] = new persegipanjang();
             System.out.println("Persegi panjang ke-"+i);
             System.out.print("Masukkan panjang: ");
@@ -14,7 +15,7 @@ public class ArrayObject22 {
             ppArray[i].lebar = sc22.nextInt();
         }
 
-        for (int i = 0; i < 3; i++) {
+        for (int i = 0; i < n; i++) {
             System.out.println("Persegi panjang ke-"+i);
             System.out.println("Panjang: "+ppArray[i].panjang+", lebar: "+ppArray[i].lebar);
         }
