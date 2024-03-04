0d1e52e0e44f04c5927006ca397fd9d6186bb92d
rezfahreza <143343397+rezfahreza@users.noreply.github.com>
2024-03-05 00:06:06 +0700

    P3_Tugas2_22

---
+++ Jobsheet3_Tugas2/InformasiMahasiswa.java
@@ -0,0 +1,17 @@
+package Jobsheet3_Tugas2;
+
+public class InformasiMahasiswa {
+    public String nama;
+    public String nim;
+    public String jenisKelamin;
+    public double ipk;
+
+    public InformasiMahasiswa(String nama, String nim, String jenisKelamin, Double ipk){
+        this.nama=nama;
+        this.nim=nim;
+        this.jenisKelamin=jenisKelamin;
+        this.ipk=ipk;
+    }
+
+
+}
---
+++ Jobsheet3_Tugas2/InformasiMahasiswaMain.java
@@ -0,0 +1,31 @@
+package Jobsheet3_Tugas2;
+import java.util.Scanner;
+
+public class InformasiMahasiswaMain {
+    public static void main(String[] args) {
+        Scanner sc22=new Scanner(System.in);
+        InformasiMahasiswa[] informasiArray=new InformasiMahasiswa[3];
+
+        for (int i = 0; i < 3; i++) {
+            System.out.println("Masukkan data mahasiswa ke-"+(i+1)+": ");
+            System.out.print("Masukkan nama: ");
+            String nm=sc22.next();
+            System.out.print("Masukkan nim: ");
+            String nim=sc22.next();
+            System.out.print("Masukkan jenis kelamin: ");
+            String jk=sc22.next();
+            System.out.print("Masukkan ipk: ");
+            double ipk=sc22.nextDouble();
+            informasiArray[i]=new InformasiMahasiswa(nm, nim, jk, ipk);
+        }
+        System.out.println();
+
+        for (int i = 0; i < 3; i++) {
+            System.out.println("Informasi mahasiswa ke-"+(i+1)+": ");
+            System.out.println("Nama: "+informasiArray[i].nama);
+            System.out.println("NIM: "+informasiArray[i].nim);
+            System.out.println("Jenis kelamin: "+informasiArray[i].jenisKelamin);
+            System.out.println("IPK: "+informasiArray[i].ipk);
+        }
+    }
+}
