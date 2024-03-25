8a969249b9a23e20124863e64e54f4956a36f07d
rezfahreza <143343397+rezfahreza@users.noreply.github.com>
2024-03-26 02:09:29 +0700

    P5_Percobaan1_Pertanyaan2_22

--- Minggu5/Faktorial.java
+++ Minggu5/Faktorial.java
@@ -4,9 +4,10 @@ public class Faktorial {
     int nilai;
 
     int faktorialBF(int n) {
-        int fakto = 1;
-        for (int i = 1; i <= n; i++) {
+        int fakto = 1, i = 1;
+        while (i <= n) {
             fakto = fakto * i;
+            i++;
         }
         return fakto;
     }
