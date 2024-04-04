e2a4e10cc1ec2ac3419b3172da0388f4d2028645
rezfahreza <143343397+rezfahreza@users.noreply.github.com>
2024-04-04 19:36:32 +0700

    P6_Percobaan1_22

---
+++ Minggu6/Mahasiswa.java
@@ -0,0 +1,21 @@
+package Minggu6;
+
+public class Mahasiswa {
+    String nama;
+    int thnMasuk, umur;
+    double ipk;
+
+    public Mahasiswa(String n, int t, int u, double i){
+        nama=n;
+        thnMasuk=t;
+        umur=u;
+        ipk=i;
+    }
+
+    void tampil(){
+        System.out.println("Nama: "+nama);
+        System.out.println("Tahun masuk: "+thnMasuk);
+        System.out.println("Umur: "+umur);
+        System.out.println("Ipk: "+ipk);
+    }
+}
