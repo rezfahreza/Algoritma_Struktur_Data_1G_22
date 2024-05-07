03543655154b1184017277ca7705d2492051a7a8
rezfahreza <143343397+rezfahreza@users.noreply.github.com>
2024-05-07 10:23:49 +0700

    P11_Percobaan1_22

---
+++ P11/Node.java
@@ -0,0 +1,15 @@
+package P11;
+
+public class Node {
+    int data;
+    Node next;
+
+    Node(int nilai, Node berikutnya) {
+        data = nilai;
+        next = berikutnya;
+    }
+
+    Node() {
+
+    }
+}
